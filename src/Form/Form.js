4cdec2ed4c756fd3655bf87ea7b75ac820d4af4c
Marina Batina <mbatina.oat@gmail.com>
2023-05-30 09:20:10 +0300

    Add empty string validation

--- src/Form/Form.js
+++ src/Form/Form.js
@@ -3,14 +3,23 @@ import React, { useState } from 'react';
 
 let count = 0;
 const Form = (props)=>{
-   const [enteredText, setEnteredText] = useState ('');
+    const [valid, setvalid] =useState(true);
+
+    const [enteredText, setEnteredText] = useState ('');
     const textChangeHandler = (event)=>{
+        if (event.target.value.trim().length > 0){
+            setvalid(true);
+        }
         setEnteredText (event.target.value);
     }
 
     const submitHandler =(event) => {
         event.preventDefault();
         const text = enteredText;
+        if (text.trim().length === 0){
+            setvalid(false);
+            return;
+        }
         setEnteredText ('');
         const newLine = {
             text: text,
@@ -21,15 +30,16 @@ const Form = (props)=>{
     }
 
     
-
     return(
         <form onSubmit ={submitHandler} className='form'>
             <div className='label'>
                 <label>To do today:</label>
             </div>   
             <div>
-                <textarea value={enteredText} onChange={textChangeHandler}> </textarea>
-            </div>    
+                <textarea value={enteredText} onChange={textChangeHandler}
+                 className={`${!valid? 'invalid':''}`}> </textarea>
+            </div> 
+            <div  className={`${!valid? 'invalid-text':''}`}>{!valid && 'The text can not be empty'}</div>   
             <div className = 'button'>
                 <button type="submit">Add Goal</button>       
             </div>
